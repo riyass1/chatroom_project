5642716bad75957b9efb2dc05795c5c2d959f0c2
riyass1 <mahdiahmadi.0915317@gmail.com>
2025-07-29 20:50:05 +0330

    fix: allow_unsafe_werkzeug added for Render

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def handle_message(data):
     emit('message', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app.run(host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True))
