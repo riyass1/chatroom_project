9da7089ac7c867026922f27b9a6a96fbaf2c8eee
riyass1 <mahdiahmadi.0915317@gmail.com>
2025-07-29 20:57:42 +0330

    Fix socketio run and host config

--- app.py
+++ app.py
@@ -26,4 +26,5 @@ def handle_message(data):
     emit('message', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app.run(host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True))
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
